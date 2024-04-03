c2465a014c2758497d25a7bc37feb4bfa5ad6b61
Aikaz <aikaz474@mail.ru>
2024-04-03 15:26:37 +0300

    5-languges

---
+++ 5-languages/5-languages.js
@@ -0,0 +1,18 @@
+const a = prompt('Введите язык, En, Ru, De, Arm')
+
+switch (true) {
+    case a === 'En':
+        console. log('Hello')
+        break
+    case a === 'Ru':
+        console. log('Привет')
+        break
+    case a === 'De':
+        console. log('Guten Tag')
+        break
+    case a === 'Arm':
+        console. log('Բարեւ')
+        break
+    default:
+        console. log('Вы не ввли нежный язык(')    
+}
