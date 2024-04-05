871f48fd14a8c6d6f93499a4cdfa5fa2eefb5127
Aikaz <aikaz474@mail.ru>
2024-04-05 10:43:51 +0300

    6-driver

---
+++ 6-driver/6-driver.js
@@ -0,0 +1,7 @@
+//Проверить, может ли пользователь сесть за руль, исходя из трех параметров//
+
+const hasLicence = true
+const age = 32
+isDrunk = false 
+
+console. log(age >= 18 && hasLicence && !isDrunk ? 'Может' : 'Не может' )
